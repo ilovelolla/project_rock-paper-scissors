6473f46aa49367bb8c1eebc981d717806421791a
Louella <louellamansueto2@gmail.com>
2025-08-03 13:42:05 +0800

    added CSS styles

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# project_rock-paper-scissors
+# Rock Paper and Scissors
+
+This game is created using HTML, CSS, and Javascript
+
+[Live preview](https://ilovelolla.github.io/project_rock-paper-scissors/)
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let humanScore = 0
-let computerScore = 0
+let humanScore = 0;
+let computerScore = 0;
+let moveCount = 0
+const maxRound = 5;
 
 function getComputerchoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -7,54 +9,85 @@ function getComputerchoice() {
     return choices[randomChoice];
 }
 
-function getHumanchoice() {
-    let pickOne = prompt("Please enter your chosen choice: Rock, Paper, or Scissors ");
-    let CpickOne = pickOne.toLowerCase();
-    return CpickOne;
-}
-console.log(getHumanchoice());
-
-
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice) {
+    
+    if(moveCount >= maxRound) return;
+    
+    const computerChoice = getComputerchoice();
+    let resultText =""
 
     if (humanChoice === computerChoice) {
-        console.log("It's a tie");
-    }
-    else if ((humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") || 
-    (humanChoice === "scissors" && computerChoice === "paper") ) {
-        console.log("You win! You just beat the Computer");
+        resultText= `It’s a tie! <br>You chose ${humanChoice} and the computer chose ${computerChoice}.`;
+
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") || 
+        (humanChoice === "scissors" && computerChoice === "paper") ) {
+        resultText = `You win! <br>You chose ${humanChoice} and the computer chose ${computerChoice}.`;
         humanScore++;
-    }
-
-    else {
-        console.log("You lose! The Computer beats you.");
+    } else if ( 
+        (humanChoice === "scissors" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "paper") || 
+        (humanChoice === "paper" && computerChoice === "scissors") ) {
+        resultText = `You lose! <br>You chose ${humanChoice} and the computer chose ${computerChoice}.`
         computerScore++;
-    }
-}
-
-function endGame () {
-    if (humanScore > computerScore) {
-        console.log('Game Over! You win!');
-    } else if (computerScore > humanScore) {
-        console.log('Game Over! You lose!');
-    }
-    else {
-        console.log("Its a tie! You both win.")
-    }
+    };
     
-    console.log(`The Final Score:\nYour Score: ${humanScore}\nComputer Score:${computerScore}`)
+
+    moveCount++;
+
+    updateText(resultText);
+    
+    if (
+        moveCount === maxRound){
+            endGame();
+    };
 }
 
-function playGame() {
-    if (humanScore < 5 && computerScore <5) {
-        let humanChoice = getHumanchoice();
-        let computerChoice = getComputerchoice();
-        playRound(humanChoice, computerChoice);
-        playGame();
+// UI HERE!!
+
+const rockButton = document.getElementById("rockBtn");
+const paperButton = document.getElementById("paperBtn");
+const scissorsButton = document.getElementById("scissorsBtn");
+const playAgain = document.getElementById('playAgain');
+
+rockButton.addEventListener ("click",function(){ 
+    playRound("rock")}) ;
+paperButton.addEventListener("click",function(){
+    playRound("paper")});
+scissorsButton.addEventListener("click",function(){
+    playRound("scissors")});
+playAgain.addEventListener("click", restartGame);
+
+function updateText(resultText) {
+    document.getElementById('computerScore').textContent = `Computer: ${computerScore}`;
+    document.getElementById('humanScore').textContent = `Player: ${humanScore}`;
+    document.getElementById('txt').innerHTML = resultText;
+};
+
+function endGame() {
+    if (humanScore > computerScore) {
+        document.getElementById("result").textContent = 
+        "You win! You just beat the Computer"
+    } else if (humanScore < computerScore) {
+    document.getElementById("result").textContent = 
+        "You lose! The computer beats you"
     } else {
-        endGame();
-    }
+    document.getElementById("result").textContent =
+    "Its a tie! You and the Computer Win"  
 }
+    document.getElementById('playAgain').disabled = false
+};
 
-playGame();
+function restartGame () {
+
+humanScore = 0;
+computerScore = 0;
+moveCount = 0;
+
+    document.getElementById('computerScore').textContent = `Computer: ${computerScore}`;
+    document.getElementById('humanScore').textContent = `Player: ${humanScore}`;
+    document.getElementById('txt').innerHTML = "Pick your weapon! <br>The first to reach 5 points claims victory";
+    document.getElementById("result").textContent = "";
+    document.getElementById('playAgain').disabled = true;
+}
